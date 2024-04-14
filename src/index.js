72ab25925eec11e2149b02754856ed39824ed11c
DanielJoel <danpersie53@gmail.com>
2024-04-14 11:03:54 +0300

    Task Lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// document.addEventListener("DOMContentLoaded", () => {
+
+//   (todo) => {
+//     document.getElementById('new-task-description').textContent = `${todo}`
+
+//   }
+// });
+
+const form = document.getElementById("create-task-form");
+const todoInput = document.getElementById("new-task-description");
+const listContainer = document.getElementById("list");
+let deleteButton
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  handlesubmit();
 });
+
+function handlesubmit() {
+  let newTodo = document.createElement('li');
+  newTodo.textContent = todoInput.value;
+
+  let xButton = document.createElement("button");
+  xButton.textContent = "X"
+
+  xButton.addEventListener('click', function () {
+    xButton.parentNode.remove()
+
+  })
+
+
+  listContainer.appendChild(newTodo);
+  newTodo.appendChild(xButton);
+
+}
+
